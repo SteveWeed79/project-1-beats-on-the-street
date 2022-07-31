8f1067c4e3b77365feb333991019bf0e5d0ea7b9
sevrinbe <sevrin.eldred@gmail.com>
2022-07-31 12:40:35 -0500

    Limited the number of searches on page to five

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,75 +1,37 @@
-
 const apiUrl = "https://api.seatgeek.com/2/events?venue.city=";
 const apiKey = "&q=music&type=concert&per_page=5&client_id=MjgwNDk1MDJ8MTY1ODc5NDk5My4zMDk5NDA2";
 var data;
 var typedLocation;
 var seatGeekApi;
-
 var savedCities = JSON.parse(localStorage.getItem("savedCitiesBand")) || [];
-
-// ^^^ TWO NAME CITIES NEED A HYPHEN like new york is New-York or Kansas-City ^^^^
 let eventData;
-
 var events = [];
+var ticketMaster = "https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&classificationName=music&city="
+var ticketMasterList = [];
+var ticketMax;
+var ticketMin;
+
+//  THIS IS THE IMPORTANT ONE
+var ticketRange;
+
+
 
 document.getElementById("btn-search").addEventListener("click", async function (event) {
     event.preventDefault()
     typedLocation = document.getElementById("typed-location").value;
-    savedCities.push(typedLocation);
+    savedCities.unshift(typedLocation);
+    savedCities = savedCities.slice(0, 5);
     localStorage.setItem("savedCitiesBand", JSON.stringify(savedCities));
-    typedLocation = typedLocation.replace(/\s/i, "-");
+    getTicketData();
+    typedLocation = typedLocation.replace(/\s/i, "-"); 0
     await getCityData();
     displayEventData();
+    console.log(savedCities);
 
-
+    // loadSearches();
 });
-
-
-// global variables to use in code
-
-
-var youtubeAPIKey = "AIzaSyBW17GJncf3PfULnlRXh0kIrceTtpfHKIs";
-
-
-var youtubeAPIKey = "AIzaSyBW17GJncf3PfULnlRXh0kIrceTtpfHKIs";
-youtubeApiURL = "https://www.googleapis.com/youtube/v3";
-var youtubeSrch =
-  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBW17GJncf3PfULnlRXh0kIrceTtpfHKIs";
-var searchInput = document.getElementsByClassName(".form-control");
-var searchBtn = document.getElementById("#searchBtn");
-// Need to add the on click function when the search button is clicked.
-searchBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  if ((searchInput = " ")) {
-    // can add a popout maybe that will say "Please enter an Artist Name/Band/Song"
-  }
-
-  var userSearch = searchInput.value;
-  console.log(userSearch);
-  //   also need to get the value of the input inside the form so I can use that in the API
-});
-
-var eventData
-var data;
-var typedLocation = "detroit";
-var apiUrl = "https://api.seatgeek.com/2/events?venue.city=";
-var apiKey = "&q=music&type=concert&per_page=5&client_id=MjgwNDk1MDJ8MTY1ODc5NDk5My4zMDk5NDA2"
-var seatGeekApi = apiUrl + typedLocation + apiKey;
-var ticketMaster = "https://app.ticketmaster.com/discovery/v2/events?apikey=7elxdku9GGG5k8j0Xm8KWdANDgecHMV0&classificationName=music&city=Dallas"
-var cityNameEL;
-var ticketMasterList;
-
-
-// ^^^ TWO NAME CITIES NEED A HYPHEN like new york is New-York or Kansas-City ^^^^
-//var typedCityField = document.getElementById("btn-search").addEventListener("click", function (event) {
-   // event.preventDefault()
-//});
-
-
-
 
 async function getCityData() {
-
     if (typedLocation != "") {
         seatGeekApi = apiUrl + typedLocation + apiKey;
         var response = await fetch(seatGeekApi)
@@ -93,24 +55,10 @@ async function getCityData() {
                 eventImg
             };
             events.push(eventData);
-
         }
         loadSearches();
-        console.log(events)
-
-
-
     };
 };
-
-
-// function displayEventData() {
-//     getCityData()
-//     console.log(eventData.eventName)
-// }
-
-
-
 
 function displayEventData() {
     var number = 0;
@@ -124,7 +72,7 @@ function displayEventData() {
             var dateTitle = htmlEvent.querySelector(".event-date");
             var urlTitle = htmlEvent.querySelector(".event-url");
             var imgTitle = htmlEvent.querySelector(".event-img");
-            var eventTime = htmlEvent.querySelector(".event-time")
+            var eventTime = htmlEvent.querySelector(".event-time");
             eventTitle.innerHTML = event.eventName;
             venueTitle.innerHTML = event.venueName;
             addressTitle.innerHTML = event.eventAddress;
@@ -138,13 +86,14 @@ function displayEventData() {
 
 function loadSearches() {
     var ul = document.getElementById("previous-searches");
-    ul.innerHTML = ""
+    ul.innerHTML = "";
     savedCities.forEach(city => {
         var li = document.createElement("li");
-        var text = document.createTextNode(city)
+        var text = document.createTextNode(city);
         li.appendChild(text);
         ul.appendChild(li);
         li.addEventListener("click", event => clickPreviousSearch(event))
+
     });
 };
 
@@ -153,53 +102,33 @@ async function clickPreviousSearch(event) {
     typedLocation = typedLocation.replace(/\s/i, "-");
     await getCityData();
     displayEventData();
-
-    var eventData = await fetch(seatGeekApi)
-        .then((response) => response.json())
-        .then((data) => (data));
-
-
-    var venueName = eventData.events[0].venue.name;
-    var eventName = eventData.events[0].short_title;
-    var eventAddress = eventData.events[0].venue.address + " " + eventData.events[0].venue.extended_address;
-    var eventDate = eventData.events[0].datatime_utc;
-    var eventTime = eventData.events[0].datatime_local;
-    var eventUrl = eventData.events[0].url;
-    var eventImg = eventData.events[0].performers[0].image;
-    console.log(eventAddress);
-
 };
 
 function getTicketmaster() {
-    fetch(ticketMaster)
+    fetch(ticketMaster + typedLocation)
         .then(response => response.json())
         .then(data => {
             console.log(data)
             ticketMasterList = data;
             console.log(ticketMasterList)
-            cityNameEL = ticketMaster._embedded[0].events[0].name
-            console.log(cityNameEL)
+            cityNameTickets = ticketMasterList._embedded.events[0].name
+            console.log(cityNameTickets)
         })
-    
-        
-}
-
-
+};
 
 loadSearches();
 
-getCityData(typedLocation);
 
+function getTicketData() {
+    fetch(ticketMaster + typedLocation)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            ticketMasterList = data;
+            console.log(ticketMasterList)
+            ticketMax = ticketMasterList._embedded.events[0].priceRanges[0].max
+            ticketMin = ticketMasterList._embedded.events[0].priceRanges[0].min
+            ticketRange = 'Ticket Prices From $' + ticketMax + '0 to $' + ticketMin + '0'
+        });
 
-// this fetches the youtubeAPI to get the search results YAY!
-// function getvideos() {
-//   fetch(youtubeSrch)
-//     .then((results) => {
-//       return results.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//     });
-// }
-// getvideos();
-
+};
