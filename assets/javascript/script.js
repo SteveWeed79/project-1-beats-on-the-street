64c0cf082a1a0dee81d82540e7a517ab53bd4ba7
Dakota Jones <dakota32jones@gmail.com>
2022-07-30 23:09:08 -0500

    Working code for videos and geekseat api's to dynamically show on the page(does not work today though, because I hit my quota limit on the youtube API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,31 +1,67 @@
-// global variables to use in code
+//Script.js
+const searchButton = document.getElementById("searchBtn");
+const input = document.getElementById("searchInput");
+const modal = document.getElementById("myModal");
 
-var youtubeAPIKey = "AIzaSyBW17GJncf3PfULnlRXh0kIrceTtpfHKIs";
-youtubeApiURL = "https://www.googleapis.com/youtube/v3";
-var youtubeSrch =
-  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBW17GJncf3PfULnlRXh0kIrceTtpfHKIs";
-var searchInput = document.getElementsByClassName(".form-control");
-var searchBtn = document.getElementById("#searchBtn");
-// Need to add the on click function when the search button is clicked.
-searchBtn.addEventListener("click", function (event) {
+// Add the event listner for click events on the search box
+searchButton.addEventListener("click", (event) => {
+  document.getElementById("music-event").innerHTML = "";
   event.preventDefault();
-  if ((searchInput = " ")) {
-    // can add a popout maybe that will say "Please enter an Artist Name/Band/Song"
+  if (input.value) {
+    searchGeekApi(input.value);
+    return;
   }
-
-  var userSearch = searchInput.value;
-  console.log(userSearch);
-  //   also need to get the value of the input inside the form so I can use that in the API
+  // If nothing is in the box pop the error modal
+  modal.style.display = "block";
+  console.log("search");
 });
 
-// this fetches the youtubeAPI to get the search results YAY!
-// function getvideos() {
-//   fetch(youtubeSrch)
-//     .then((results) => {
-//       return results.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//     });
-// }
-// getvideos();
+// Add a on click event to close the modal
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Helper function to create youtube content in the cards, takes an artist name and appends the search results
+// To the artist name div
+async function searchYouTube(artistName) {
+  const youtubeSrch = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBW17GJncf3PfULnlRXh0kIrceTtpfHKIs&q=${artistName}`;
+  const response = await fetch(youtubeSrch);
+  const body = await response.json();
+
+  document.getElementById(`youtube-${artistName}`).innerHTML += `
+            <div class="img1">
+              <img src="${body.items[0].snippet.thumbnails.medium.url}" alt="thumbnail-1" />
+            </div>
+            <div class="main-text place-content-center">
+              <h2>${body.items[0].snippet.channelTitle}</h2>
+              <iframe width="420" height="345" src="https://www.youtube.com/embed/${items.id.videoId}">
+            </div>`;
+}
+
+// Search the Seat Geek API for events by city
+// Calls the youtube method for each artist in each event
+async function searchGeekApi(searchTerm) {
+  const seatGeekApi = `https://api.seatgeek.com/2/events?venue.city=${searchTerm}&q=music&type=concert&per_page=5&client_id=MjgwNDk1MDJ8MTY1ODc5NDk5My4zMDk5NDA2`;
+  const response = await fetch(seatGeekApi);
+  const body = await response.json();
+  body.events.map((event) => {
+    document.getElementById("music-event").innerHTML += `
+    <div class="card p-6">
+    <div class="card-body">
+    <h5 class="card-title">${event.short_title}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${event.venue.name}</h6>
+    ${event.performers
+      .map((performer) => `<div id="youtube-${performer.name}"></div>`)
+      .join("")}
+  </div>
+</div>
+          
+      `;
+  });
+
+  body.events.forEach((e) =>
+    e.performers.forEach((p) => searchYouTube(p.name))
+  );
+}
